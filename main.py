e6534aa3b61352abd3e12b87f78e10488d4151cd
Klaszlo5 <gplays131@gmail.com>
2023-01-31 10:53:57 +0100

    python

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 #semmi megoldas
 def eredmeny():
-    napok = 20
-    jatekos = 1
+    napok = [20,21,23]
+    jatekos = [1,2,3]
+    for jatekos in range(napok):
+        napok[jatekos] = napok[jatekos]
     if napok > 21 and jatekos < 2:
         print("napok jatekos")
+
 #megjegyzes
 
