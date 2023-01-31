46a4b8837e6401520b83544c290d65ea8203b65c
Klaszlo5 <gplays131@gmail.com>
2023-01-31 11:41:53 +0100

    python

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 #semmi megoldas
-def eredmeny():
-    napok = [20,21,23]
-    jatekos = [1,2,3]
+def pontok(lap)->int:
+    pontok:int = 0
+    for i in range(len(lap)):
+        pontok += lap[i];
+    return pontok
+def eredmeny(egy, ketto):
+    napok = pontok(egy)
+    jatekos = pontok(ketto)
     for jatekos in range(napok):
-        napok[jatekos] = napok[jatekos]
+        napok[jatekos] += napok[jatekos]
     if napok > 21 and jatekos < 2:
         print("napok jatekos")
-
 #megjegyzes
 
