0d70663f47ed5dfc46009800d64874a0f4046344
Klaszlo5 <94529898+Klaszlo5@users.noreply.github.com>
2023-01-31 12:05:06 +0100

    allandoan lefagy a pycharm

--- main.py
+++ main.py
@@ -1,4 +1,31 @@
 #semmi megoldas
-def eredmeny():
-
+def pontok(lap)->int:
+    pontok:int = 0
+    for i in range(len(lap)):
+        pontok += lap[i];
+    return pontok
+def eredmeny(egy, ketto):
+    napok = pontok(egy)
+    jatekos = pontok(ketto)
+    for jatekos in range(napok):
+        napok[jatekos] += napok[jatekos]
+    if napok > 21 and jatekos > 21:
+        print("nyer")
+    elif napok > 21:
+        return "Gép vesztett"
+    else:
+        return "Játékos vesztett"
+def teszt():
+    tesztek()
+def tesztek():
+    napok=[2,3,10]
+    jatekos=[20,3]
+    eredmeny="nyert"
+    kapott=eredmeny(napok,jatekos)
+    if kapott == eredmeny:
+        print("sikeres")
+    else:
+        print("nem sikerult")
+tesz()
 #megjegyzes
+
