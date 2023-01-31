812350bb2a3770baef70603d6044ba3b11cbd849
Klaszlo5 <94529898+Klaszlo5@users.noreply.github.com>
2023-01-31 12:16:11 +0100

    py

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 #semmi megoldas
-def eredmeny():
-    napok = [20,21,23]
-    jatekos = [1,2,3]
+def pontok(lap)->int:
+    pontok:int = 0
+    for i in range(len(lap)):
+        pontok += lap[i];
+    return pontok
+def eredmeny(egy, ketto):
+    napok = pontok(egy)
+    jatekos = pontok(ketto)
     for jatekos in range(napok):
-        napok[jatekos] = napok[jatekos]
-    if napok > 21 and jatekos < 2:
-        print("napok jatekos")
-
+        napok[jatekos] += napok[jatekos]
+    if napok > 21 and jatekos > 21:
+        print("nyer")
+    elif napok > 21:
+        return "Gép vesztett"
+    else:
+        return "Játékos vesztett"
+def teszt():
+    tesztek()
+def tesztek():
+    napok=[2,3,10]
+    jatekos=[20,3]
+    eredmeny="nyert"
+    kapott=eredmeny(napok,jatekos)
+    if kapott == eredmeny:
+        print("sikeres")
+    else:
+        print("nem sikerult")
+tesz()
 #megjegyzes
 
